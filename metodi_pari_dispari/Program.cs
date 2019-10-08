dc4b159e5a9b1c5a29d325987057c97cf03abeb2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-08 09:41:57 +0200

    Add project files.

---
+++ metodi_pari_dispari/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodi_pari_dispari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primi cento numeri pari:");
+            NumeriPari();
+            Console.WriteLine("\n");
+            Console.WriteLine("Primi cento numeri dispari:");
+            NumeriDispari();
+
+
+            //Task.Factory.StartNew(NumeriPari);
+            //Task.Factory.StartNew(NumeriDispari);
+
+
+            Console.ReadLine();
+        }
+
+        public static void NumeriPari()
+        {
+            for (int i = 0; i <= 200; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+
+        public static void NumeriDispari()
+        {
+            for (int i = 0; i <= 200; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+
+    }
+}
